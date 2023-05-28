d1b1f29a1820bbfa1facbb084fd12ba1d117548f
xoghk <xoghk@DESKTOP-NS3GJ5V>
2023-05-28 18:55:03 +0900

    hw_week_12

---
+++ solveVelocityInPerifocalFrame.m
@@ -0,0 +1,12 @@
+function answer = solveVelocityInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly)
+%unit : km, degree
+
+p = semimajor_axis * (1 - eccentricity^2); % p = a(1-e^2)
+
+r = p/(1 + eccentricity*cosd(true_anomaly)); % r = p/(1+e*cos(theta))
+
+mu =  3.986004418 * 10^5; % unit : km^3/s^2
+
+answer = sqrt(mu/p) * [-sind(true_anomaly); eccentricity + cosd(true_anomaly); 0];
+
+end
